7454e5ffc5219bbb27b40425bde33a4bc25f4011
smriti goel <smriti@crstl.so>
2023-12-01 16:29:34 +0530

    added eslint and prettier and upgraded dep

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,25 @@
 module.exports = {
-  root: true,
   env: {
-    node: true,
-    es6: true,
+    browser: true,
+    es2021: true,
   },
-  parserOptions: { ecmaVersion: 8 }, // to enable features such as async/await
-  ignorePatterns: ["node_modules/*", ".next/*", ".out/*", "!.prettierrc.js"], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
-  extends: ["react-app", "eslint:recommended"],
-  plugins: ["prettier"],
-  overrides: [
-    // This configuration will apply only to TypeScript files
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      parser: "@typescript-eslint/parser",
-      settings: { react: { version: "detect" } },
-      env: {
-        browser: true,
-        node: true,
-        es6: true,
-      },
-      extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended", // TypeScript rules
-        "plugin:react/recommended", // React rules
-        "plugin:react-hooks/recommended", // React hooks rules
-        "plugin:jsx-a11y/recommended", // Accessibility rules
-        "prettier/@typescript-eslint", // Prettier plugin
-        "plugin:prettier/recommended", // Prettier recommended rules
-      ],
-      rules: {
-        // We will use TypeScript's types for component props instead
-        "react/prop-types": "off",
-
-        // No need to import React
-        "react/react-in-jsx-scope": "off",
-
-        // Why would you want unused vars?
-        "@typescript-eslint/no-unused-vars": ["warn"],
-
-        // I suggest this setting for requiring return types on functions only where useful
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/explicit-module-boundary-types": [0],
-        "prettier/prettier": [
-          "warn",
-          { endOfLine: "auto" },
-          { usePrettierrc: true },
-        ], // Includes .prettierrc.js rules
-      },
-    },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+  },
 };
